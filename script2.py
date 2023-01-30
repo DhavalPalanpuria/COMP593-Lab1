271f7b75453d426ee01d300e864f47e86d12df90
DhavalPalanpuria <122965035+DhavalPalanpuria@users.noreply.github.com>
2023-01-30 02:09:34 -0500

    dk what i did
    I did something wrong

--- script2.py
+++ script2.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-print("Even through the darkest days, this fire burns always")
-=======
-print("Toaster is broken.")
->>>>>>> parent of 660981b (Update script2.py)
+print("Slipknot's last album is lit")
+print("The end so far")
